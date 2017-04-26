9ec60d7117a8be9ba7aa80ce970b839b43348138
David A. Shamma <aymanshamma@gmail.com>
2017-04-26 21:45:17 +0200

    Flake8 & Pep8

--- flickr-savr.py
+++ flickr-savr.py
@@ -1,12 +1,13 @@
 import argparse
-import code
 import flickrapi
-import json
+# import code
+# import json
 import os
 import pyexiv2
-import time
+# import time
 import urllib
 import webbrowser
+
 
 class FlickrSavr(object):
     """This is a digital preservation experiment.  It crawls your Flickr
@@ -28,15 +29,15 @@ class FlickrSavr(object):
 
     """
 
-    def __init__(self, 
-                 key, 
-                 secret, 
+    def __init__(self,
+                 key,
+                 secret,
                  nsid,
                  basepath,
                  verbose,
                  force):
         """do
-        
+
         Arguments:
         - `key`:
         - `secret`:
@@ -44,19 +45,19 @@ class FlickrSavr(object):
         - `basepath`:
         - `verbose`:
         - `force`:
-        """        
-        ## auth
+        """
+        # auth
         self.api_key = key
         self.api_secret = secret
         self.nsid = nsid
         self.basepath = os.path.join(basepath, "nsid", self.nsid)
         self.verbose = not verbose
         self.force = force
-        
+
         self.photo_count = 0
         self.photo_page = 0
         self.photo_total = 0
-        self.per_page = 500        
+        self.per_page = 500
 
         self.flickr = flickrapi.FlickrAPI(self.api_key,
                                           self.api_secret,
@@ -65,27 +66,27 @@ class FlickrSavr(object):
         # Only do this if we don't have a valid token already
         if not self.flickr.token_valid(perms=unicode('write')):
 
-          # Get a request token
-          self.flickr.get_request_token(oauth_callback='oob')
+            # Get a request token
+            self.flickr.get_request_token(oauth_callback='oob')
 
-          # Open a browser at the authentication URL. Do this however
-          # you want, as long as the user visits that URL.
-          authorize_url = self.flickr.auth_url(perms=unicode('write'))
-          webbrowser.open_new_tab(authorize_url)
+            # Open a browser at the authentication URL. Do this however
+            # you want, as long as the user visits that URL.
+            authorize_url = self.flickr.auth_url(perms=unicode('write'))
+            webbrowser.open_new_tab(authorize_url)
 
-          # Get the verifier code from the user. Do this however you
-          # want, as long as the user gives the application the code.
-          verifier = unicode(raw_input('Verifier code: '))
+            # Get the verifier code from the user. Do this however you
+            # want, as long as the user gives the application the code.
+            verifier = unicode(raw_input('Verifier code: '))
 
-          # Trade the request token for an access token
-          self.flickr.get_access_token(verifier)
+            # Trade the request token for an access token
+            self.flickr.get_access_token(verifier)
 
-        ## dir
+        # dir
         if not os.path.exists(self.basepath):
             os.makedirs(self.basepath)
 
-        ## search: this has a 'pages' field with how many photos left
-        ## accounting per page..max is 500
+        # search: this has a 'pages' field with how many photos left
+        # accounting per page..max is 500
         extras = "%s,%s,%s,%s,%s,%s,%s,%s," % ("url_o",
                                                "geo",
                                                "tags",
@@ -95,7 +96,7 @@ class FlickrSavr(object):
                                                "date_upload",
                                                "date_taken")
 
-        photos = self.flickr.photos_search(user_id=self.nsid, 
+        photos = self.flickr.photos_search(user_id=self.nsid,
                                            per_page=self.per_page,
                                            extras=extras)
         photos = photos['photos']
@@ -107,13 +108,13 @@ class FlickrSavr(object):
             self.photo_count = i
             photo = photos['photo'][i]
             self.get_photo(photo)
-            
-        ## this page counter is for the next page actually
+
+        # this page counter is for the next page actually
         for page in range(photos['pages'])[1:]:
-            self.photo_page = page                    
-            photos = self.flickr.photos_search(user_id=self.nsid, 
+            self.photo_page = page
+            photos = self.flickr.photos_search(user_id=self.nsid,
                                                page=str(page),
-                                               per_page=self.per_page, 
+                                               per_page=self.per_page,
                                                extras=extras)
             photos = photos['photos']
             for i in range(photos['perpage']):
@@ -124,13 +125,13 @@ class FlickrSavr(object):
 
     def get_photo(self, photo):
         """Get the data around a photo.
-        
+
         Arguments:
         - `self`:
         - `photo`:
         """
 
-        ## Check if we have it already
+        # Check if we have it already
         url = photo['url_o']
         fname = os.path.join(self.get_date_path(photo),
                              photo['id'] + url[-4:])
@@ -139,7 +140,7 @@ class FlickrSavr(object):
             self.print_status_count(True)
             return
 
-        ## get image
+        # get image
         resp = urllib.urlopen(url)
         image_data = resp.read()
 
@@ -149,12 +150,12 @@ class FlickrSavr(object):
         f.close()
         self.print_status_count()
 
-        ## get more metadata
+        # get more metadata
         favorites = []
         favs = self.flickr.photos_getFavorites(photo_id=photo['id'])
         for person in favs['photo']['person']:
             s = "Flickr.favorites:%s:%s:%s" % (person['username'],
-                                               person['nsid'], 
+                                               person['nsid'],
                                                person['favedate'])
             favorites.append(s)
 
@@ -170,41 +171,40 @@ class FlickrSavr(object):
         except:
             pass
         pools = []
-        ## this call failed...maybe try to wrap all the bad ids
-        ## somewhere for later handling
-        pool = self.flickr.photos_getAllContexts(photo_id=photo['id']);
-        try: 
+        # this call failed...maybe try to wrap all the bad ids
+        # somewhere for later handling
+        pool = self.flickr.photos_getAllContexts(photo_id=photo['id'])
+        try:
             for sets in pool['set']:
                 c = "Flickr.sets:%s:$s" % (sets['title'],
                                            sets['id'])
                 pools.append(c)
         except:
             pass
-        ## write exif
+        # write exif
         metadata = pyexiv2.ImageMetadata(fname)
         metadata.read()
         key = 'Iptc.Application2.Keywords'
         vals = ["Flickr.id:%s" % photo['id'],
                 "Flickr.owner:%s" % self.nsid,
-                "Flickr.title:%s" % photo['title'],	
-                "Flickr.description:%s" % photo['description']['_content'],	
+                "Flickr.title:%s" % photo['title'],
+                "Flickr.description:%s" % photo['description']['_content'],
                 "Flickr.isfamily:%d" % photo['isfamily'],
                 "Flickr.isfriend:%d" % photo['isfriend'],
                 "Flickr.views:%s" % photo['views'],
                 "Flickr.date_upload:%s" % photo['dateupload'],
                 "Flickr.date_taken:%s" % photo['datetaken']]
-        if (photo.has_key('latitude') and
-            photo.has_key('longitude')):
-            coords = ["Flickr.latitude:%s" % photo['latitude'],	
+        if ('latitude' in photo and 'longitude' in photo):
+            coords = ["Flickr.latitude:%s" % photo['latitude'],
                       "Flickr.longitude:%s" % photo['longitude']]
             vals += coords
-        if (photo.has_key('accuracy')):    
+        if ('accuracy' in photo):
             accuracy = ["Flickr.accuracy:%s" % photo['accuracy']]
             vals += accuracy
-        if (photo.has_key('tags')):    
+        if ('tags' in photo):
             tags = ["Flickr.tags:%s" % photo['tags']]
             vals += tags
-        ## join all into vals
+        # join all into vals
         vals.extend(favorites)
         metadata[key] = vals
         metadata.iptc_charset = 'utf-8'
@@ -230,11 +230,12 @@ class FlickrSavr(object):
             condition = "(file exists)"
         if self.verbose:
             self.print_status("%s / %s %s" %
-                              (1+ self.photo_count + (self.per_page *
-                                                      self.photo_page),
+                              (1 + self.photo_count + (self.per_page *
+                                                       self.photo_page),
                                self.photo_total,
                                condition))
         return
+
 
 def main():
     desc = 'Download a Flickr Account.'
@@ -260,6 +261,7 @@ def main():
     args = parser.parse_args()
     FlickrSavr(args.key, args.secret, args.nsid, args.basepath[0],
                args.quiet, args.force)
+
 
 if __name__ == "__main__":
     main()
